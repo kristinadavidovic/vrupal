c185c17fbf6c972dfda42d2fe71ec8689f90c1e8
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2020-04-03 16:55:29 +0200

    Add the TeamMembers component.

---
+++ web/themes/custom/vrupal/src/js/components/TeamMembers.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Hola!</div>
+</template>
+
+<script>
+export default {
+    name: 'team-members',
+    data() {
+        return {
+
+        };
+    },
+}
+</script>
--- web/themes/custom/vrupal/src/main.js
+++ web/themes/custom/vrupal/src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue';
 
+// components
+import TeamMembers from './js/components/TeamMembers';
+
 // Stylings
 import './scss/app.scss'; // whole site stylings
 
 Vue.config.productionTip = false;
+
+Vue.component('team-members', TeamMembers);
+
+if (document.querySelectorAll('#team-members').length > 0) {
+    new Vue({
+        el: '#team-members',
+        comments: true,
+    });
+}
